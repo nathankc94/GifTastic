be0549296cac0c37fa3e6d45813b5fb2acedd02e
alongkot <nathankc94@gmail.com>
2019-03-29 13:29:37 -0500

    pseudo code

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # GifTastic
 WzfyqAFwWwijOVAklUZAy7gJo71I6jSS
-https://api.giphy.com/v1/gifs/search?api_key=WzfyqAFwWwijOVAklUZAy7gJo71I6jSS&q=animals&limit=10&offset=0&rating=PG-13&lang=en
+https://api.giphy.com/v1/gifs/search?api_key=WzfyqAFwWwijOVAklUZAy7gJo71I6jSS&q=Mbappe&limit=10&offset=0&rating=PG-13&lang=en
+
+**This project uses GIPHY API to make a dynamic web page that populates with gifs of your choice. I used JavaScript and jQuery to change the HTML of this site.**
+
+* link to the website.
+* The theme of this project is Soccer.
+
+# Pseudo Code
+
+1. create array to pass in topics
+1. create buttons and disply onto html (06-Ajax/Activies/10-WorkingMovieApp)
+1. capture user input
+
+
+
+
+
+
--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,48 @@
+// create array to pass in topics
 var topics = ["Messi", "Ronaldo", "Mbappe", "Neymar"];
 
+// send request to GIPHY API
+function requestApi() {
+    var soccer = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + soccer + "&limit=10&offset=0&rating=PG-13&lang=en&api_key=WzfyqAFwWwijOVAklUZAy7gJo71I6jSS";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        $("#content-view").text(JSON.stringify(response));
+        
+    });
+    
+}
+
+// create button and disply onto html
 function renderButton(){
     $("#button-view").empty();
 
-// create button and disply on to html
     
     for (var i=0; i < topics.length; i++){
         var soccerButtons = $("<button>");
-        soccerButtons.addClass("soccer-btn");
-        soccerButtons.attr("data-name", topics[i]);
-        soccerButtons.text(topics[i]);
+        soccerButtons.addClass("soccer-btn").attr("data-name", topics[i]).text(topics[i]);
         $("#button-view").append(soccerButtons);
 
     }
-
+ 
 
 
 }
-
+// capture user input
 $("#soccer-search").on("click", function(event){
     
     event.preventDefault();
+
     var soccer = $("#soccer-input").val().trim();
     topics.push(soccer);
+
     renderButton();
 
 
 });
+
+$(document).on("click", ".soccer-btn", requestApi);
 renderButton();
