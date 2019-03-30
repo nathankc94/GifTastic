3bc4fb15822af3ca7b6100f65e40e342aba46e96
alongkot <nathankc94@gmail.com>
2019-03-29 19:28:26 -0500

    added gif function

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # GifTastic
-WzfyqAFwWwijOVAklUZAy7gJo71I6jSS
-https://api.giphy.com/v1/gifs/search?api_key=WzfyqAFwWwijOVAklUZAy7gJo71I6jSS&q=Mbappe&limit=10&offset=0&rating=PG-13&lang=en
 
 **This project uses GIPHY API to make a dynamic web page that populates with gifs of your choice. I used JavaScript and jQuery to change the HTML of this site.**
 
-* link to the website.
-* The theme of this project is Soccer.
+* **Link** https://nathankc94.github.io/GifTastic/
+* Resources (06-Ajax/Activies-10-WorkingMovieApp/ 14-DynamicElements/ 15-PausingGifs)
 
 # Pseudo Code
 
 1. create array to pass in topics
-1. create buttons and disply onto html (06-Ajax/Activies/10-WorkingMovieApp)
-1. capture user input
-
+1. create button and disply all the topics in the array onto html
+1. send request to GIPHY API and grab data-name property value from button
+1. create img div, grab rating, still gif, and animated gif 
+1. disply the initial gif once clicked on button (still gif)
+1. add data status and set it to still
+1. once click on a gif make it so it changes from still to animated
+1. capture text from user input and push into topics array
+ 
 
 
 
--- assets/app.js
+++ assets/app.js
@@ -1,48 +1,73 @@
 // create array to pass in topics
 var topics = ["Messi", "Ronaldo", "Mbappe", "Neymar"];
 
-// send request to GIPHY API
-function requestApi() {
-    var soccer = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + soccer + "&limit=10&offset=0&rating=PG-13&lang=en&api_key=WzfyqAFwWwijOVAklUZAy7gJo71I6jSS";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        $("#content-view").text(JSON.stringify(response));
-        
-    });
-    
-}
-
-// create button and disply onto html
-function renderButton(){
+// create button and disply all the topics in the array onto html
+function createButtons() {
     $("#button-view").empty();
-
     
-    for (var i=0; i < topics.length; i++){
+    
+    for (var i = 0; i < topics.length; i++) {
         var soccerButtons = $("<button>");
         soccerButtons.addClass("soccer-btn").attr("data-name", topics[i]).text(topics[i]);
         $("#button-view").append(soccerButtons);
-
+        
+        
     }
- 
-
-
 }
-// capture user input
-$("#soccer-search").on("click", function(event){
-    
+createButtons();
+
+$(document).on("click", ".soccer-btn", function () {
+
+
+    // send request to GIPHY API and grab data-name property value from button
+
+    var soccer = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + soccer + "&limit=10&offset=0&rating=PG-13&lang=en&api_key=WzfyqAFwWwijOVAklUZAy7gJo71I6jSS";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // create img div, grab rating, still gif, and animated gif  
+        var result = response.data;
+        for (var j = 0; j < result.length; j++) {
+            var still = result[j].images.fixed_height_still.url;
+            var animatedGif = result[j].images.fixed_height.url;
+            // disply the initial gif once clicked on button (still gif)
+            // add data status and set it to still
+            var gifImage = $('<img>').attr("src", still).attr('data-animate', animatedGif).attr('data-still', still).attr('data-state', 'still');
+            $('#content-view').prepend(gifImage);
+            // once click on a gif make it so it changes from still to animated
+            gifImage.on('click', activateGif);
+            // add rating
+            var rating = $("<p>").text(result[j].rating);
+            $('#content-view').prepend(rating);
+        }
+    });
+
+    //  method to change data state
+    function activateGif() {
+        var state = $(this).attr('data-state');
+        
+        if (state === 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    }
+
+});
+
+
+// capture text from user input and push into topics array
+$("#soccer-search").on("click", function (event) {
+
     event.preventDefault();
 
     var soccer = $("#soccer-input").val().trim();
     topics.push(soccer);
 
-    renderButton();
-
-
+    createButtons();
 });
-
-$(document).on("click", ".soccer-btn", requestApi);
-renderButton();
